4c0b84559bcd7c06e47dd15d5245596c0a74fe95
Achyut Ghosh <achyutghosh06@gmail.com>
2019-12-29 21:58:28 +0530

    Add project files.

---
+++ Xamar Notes/Xamar Notes/Model/Note.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SQLite;
+
+namespace Xamar_Notes.Model
+{
+    public class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
